d7aa36eae932e2a14b67f842ff091c62b3bf5af3
Сергей <gan7er@yandex.ru>
2021-02-20 11:34:17 +0300

    Создание перечиления

---
+++ Lesson-2-6/Lesson-2-6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_6
+{
+    class Program
+    {
+        [Flags]
+        enum Days
+
+        {
+            Понедельник = 0b0000001,
+            Вторник = 0b0000010,
+            Среда = 0b0000100,
+            Четверг = 0b0001000,
+            Пятница = 0b0010000,
+            Суббота = 0b0100000,
+            Воскресенье = 0b1000000
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
